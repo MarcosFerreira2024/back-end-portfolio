bd5fa8f66e0ee8113c7bde7e0840f4db60ad7690
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2024-12-31 13:05:41 -0300

    importando o express

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import dotenv from "dotenv";
+import rout from "./routes/routes";
+import { HTTP_STATUS } from "./services/httpStatus";
+
+dotenv.config();
+const porta = process.env.PORTA || 3000; 
+
+const server = express();
+
+server.use(express.json());
+
+server.use(express.urlencoded({ extended: true }));
+
+server.use(rout);
+
+server.use((req, res) => {
+  res.status(HTTP_STATUS.NOT_FOUND).json({ route: "Not found" }); 
+});
+
+server.listen(porta, () => {
+  console.log(`Server running on port http://localhost:${porta}/`);
+});
