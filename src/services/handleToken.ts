9d5536477f49c690a3e1fccf6fa900bb3280b321
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2024-12-31 13:06:55 -0300

    verificando/pegando token

---
+++ src/services/handleToken.ts
@@ -0,0 +1,28 @@
+import { Request } from "express";
+import JWT from "jsonwebtoken"
+
+export const verifyToken = async (token: string) => {
+
+  try {
+    const verifiedData = JWT.verify(token, process.env.MY_SECRET_KEY as string);
+
+    const decoded = verifiedData as { email: string };
+
+    return decoded.email; 
+    
+  } 
+  catch {
+    return null;
+    
+  }
+};
+
+export const getToken = async (authorization:string)=>{
+  
+  if (authorization&&authorization.startsWith("Bearer")){
+    const token =  (authorization as string).split(" ")[1]
+
+    return token
+
+  }
+}
